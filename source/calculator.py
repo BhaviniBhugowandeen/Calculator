5433140494e48122df5cb307b94cd787b319fe73
Bhavini Bhugowandeen <akshitabhugowandeen@gmail.com>
2023-09-21 11:06:00 +0400

    Thirteenth commit

--- source/calculator.py
+++ source/calculator.py
@@ -1,9 +1,11 @@
+from typing import Union
+ 
 '''def add(a, b):
     """Compute and return the sum of two numbers."""
     return float(a + b)'''
     
-def add(a, b):
-    """Compute and return the sum of two numbers.
+'''def add(a, b):
+    Compute and return the sum of two numbers.
 
     Args:
         a (float): A number representing the first addend in the addition.
@@ -11,5 +13,24 @@ def add(a, b):
 
     Returns:
         float: A number representing the arithmetic sum of `a` and `b`.
+    
+    return float(a + b)'''
+    
+
+def add(a: Union[float, int], b: Union[float, int]) -> float:
+    """Compute and return the sum of two numbers.
+
+    Examples:
+        >>> add(4.0, 2.0)
+        6.0
+        >>> add(4, 2)
+        6.0
+
+    Args:
+        a: A number representing the first addend in the addition.
+        b: A number representing the second addend in the addition.
+
+    Returns:
+        A number representing the arithmetic sum of `a` and `b`.
     """
     return float(a + b)
