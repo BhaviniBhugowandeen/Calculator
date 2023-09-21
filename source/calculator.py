d8f79b58b9e356df3adfdd6bb86a2f157d359d1b
Bhavini Bhugowandeen <akshitabhugowandeen@gmail.com>
2023-09-21 11:03:44 +0400

    Twelfth commit

--- source/calculator.py
+++ source/calculator.py
@@ -1,3 +1,15 @@
-def add(a, b):
+'''def add(a, b):
     """Compute and return the sum of two numbers."""
+    return float(a + b)'''
+    
+def add(a, b):
+    """Compute and return the sum of two numbers.
+
+    Args:
+        a (float): A number representing the first addend in the addition.
+        b (float): A number representing the second addend in the addition.
+
+    Returns:
+        float: A number representing the arithmetic sum of `a` and `b`.
+    """
     return float(a + b)
