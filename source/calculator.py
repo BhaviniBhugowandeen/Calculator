eb9d8a4f2f5452fefa5b1a584a640840a3311102
Bhavini Bhugowandeen <akshitabhugowandeen@gmail.com>
2023-09-21 11:10:44 +0400

    Fourteenth commit

--- source/calculator.py
+++ source/calculator.py
@@ -1,4 +1,20 @@
 from typing import Union
+
+
+# calculator/calculations.py
+
+"""Provide several sample math calculations.
+
+This module allows the user to make mathematical calculations.
+
+The module contains the following functions:
+
+- `add(a, b)` - Returns the sum of two numbers.
+- `subtract(a, b)` - Returns the difference of two numbers.
+- `multiply(a, b)` - Returns the product of two numbers.
+- `divide(a, b)` - Returns the quotient of two numbers.
+"""
+
  
 '''def add(a, b):
     """Compute and return the sum of two numbers."""
